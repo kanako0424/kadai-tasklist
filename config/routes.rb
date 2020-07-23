f8181989ba38f6f3f7a5028adb160555a410f3dd
kanako0424 <budoukan0424@gmail.com>
2020-07-23 11:07:43 +0000

    cannot use others tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  
   get 'users/new'
   get 'users/create'
   
